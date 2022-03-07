eca9de241d28e6b826d48e6dde9e6847b4a6b6aa
aidan-starke <starke.aidan@gmail.com>
2022-03-07 17:16:09 +1300

    finishing touches

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth";
 import TwitterProvider from "next-auth/providers/twitter";
 
-const { NEXT_PUBLIC_NEXTAUTH_SECRET, NEXT_PUBLIC_TWITTER_ID, NEXT_PUBLIC_TWITTER_SECRET }: any = process.env;
+const {
+	NEXT_PUBLIC_NEXTAUTH_SECRET,
+	NEXT_PUBLIC_TWITTER_ID,
+	NEXT_PUBLIC_TWITTER_SECRET,
+}: any = process.env;
 
 export default NextAuth({
 	secret: NEXT_PUBLIC_NEXTAUTH_SECRET,
